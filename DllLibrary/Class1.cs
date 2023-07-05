9cfe980536f25fcf660f4e738c2446096fec3785
307 <307@307-1>
2023-07-05 10:27:57 +0300

    Добавьте файлы проекта.

---
+++ DllLibrary/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DllLibrary
+{
+    public class Team
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public List<Player> Library = new List<Player>();
+        public Team(string nm, string ad)
+        {
+            Name = nm;
+            Address = ad;
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Команда: название = {Name}, адрес = {Address}");
+        }
+    }
+
+    public class Player : IComparable<Player>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Player(string nm, string des)
+        {
+            Name = nm;
+            Description = des;
+        }
+        public void Show()
+        {
+            Console.WriteLine(
+                 $"Игрок: Имя = {Name}, описание = {Description}");
+        }
+    }
+    interface IComparable
+    {
+        public int CompareTo(Player other)
+        {
+            // пробег текущего авто больше пробега переданного
+            if (this.Name > other.Name) return 1;
+            // пробег текущего авто меньше пробега переданного
+            if (this.Name < other.Name) return -1;
+            else return 0; // пробеги автомобилей равны 
+        }
+
+    }
+
+    interface IComparable<class>
+    {
+    CompareTo(class other);
+    }
+
+
+}
---
+++ MainProgram/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using DllLibrary;
+
+namespace MainProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Team t = new Team("Орлы", "г.Москва");
+            t.Show();
+
+            Player p1 = new Player("Кузнецов К.О.","Быстро бегает");
+            t.Library.Add(p1);
+            Player p2 = new Player("Васильев С.И.","Выносливый");
+            t.Library.Add(p2);
+            Player p3 = new Player("Николаев В.А.", "Сильный");
+            t.Library.Add(p3);
+            Player p4 = new Player("Денисов С.О.", "Хорошо ловит мячи");
+            t.Library.Add(p4);
+            Player p5 = new Player("Федотов М.Ю.", "Лидер");
+            t.Library.Add(p5);
+
+            t.Library.Sort();
+            foreach (Player player in t.Library)
+            {
+                t.Show();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
